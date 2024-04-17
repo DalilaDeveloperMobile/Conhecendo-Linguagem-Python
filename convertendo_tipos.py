07f1de573fabc534781c3f29cb5cadd908caadcd
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-17 12:46:33 -0300

    code: convertendo tipos

---
+++ convertendo_tipos.py
@@ -0,0 +1,26 @@
+# Converção de Tipos:
+
+# float para int
+print(int(1.97348728)) # Result: 1
+
+# str para int
+print(int("10")) # Result: 10
+
+# str para float
+print(float("10.10")) # Result: 10.1
+
+# int para float
+print(float(100)) # Result: 100.0
+
+# Exibir o tipo de conversão:
+valor = 10
+valor_str = str(valor)
+
+print(type(valor)) # Result: <class 'int'>
+print(type(valor_str)) # Result: <class 'str'>
+
+# Divisão para float
+print(100 / 2) # Result: 50.0
+
+# Divisão para int
+print(100 // 2) # Result: 50
