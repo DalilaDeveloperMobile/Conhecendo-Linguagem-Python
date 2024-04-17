d9b45058b8d00568b8dceca3fdb45cff8b1a7437
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-17 12:35:44 -0300

    code: tipos de dados

---
+++ tipos-de-dados.py
@@ -0,0 +1,14 @@
+# Tipos de Dados:
+
+# int
+print(11 + 10 + 1000), # 1021
+
+# float
+print(1.5 + 1 + 0.5), # 3.0
+
+# bool
+print(True) # True
+print(False) # False
+
+# str
+print("Python") # Python
