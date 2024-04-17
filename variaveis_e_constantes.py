858f22aa52e64d36cd2e8e2d87d2da0d569eab31
Dalila Custódio <dalila.dalila70@gmail.com>
2024-04-17 12:44:42 -0300

    code: variaveis e constantes

---
+++ variaveis_e_constantes.py
@@ -0,0 +1,18 @@
+# Variaveis N Constants:
+
+nome = 'Dalila'
+idade = 28
+# Resultado: Dalila 28
+
+nome, idade = "Ismael", 22 
+# Resultado: Ismael 22
+
+# Variaveis Constants:
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+print(BRAZILIAN_STATES) 
+# Resultado: ['SP', 'RJ', 'SC', 'RS']
+
+# Forma de Escrever Variaveis:
+limite_saque_diario = 1000
+
+print(nome, idade)
